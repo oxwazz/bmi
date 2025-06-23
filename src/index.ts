816e00c533b83d5d08e74ff12dde5f140b094dd3
oxwazz <muhammad.rahmahalim@gmail.com>
2025-06-23 14:43:20 +0700

    feat: apac calculation

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,49 @@
-// NOTE: calculation based on https://www.who.int/europe/news-room/fact-sheets/item/a-healthy-lifestyle---who-recommendations
+// NOTE:
+// - fn apac() calculation based on https://iris.who.int/handle/10665/206936
+// - fn who() calculation based on https://www.who.int/europe/news-room/fact-sheets/item/a-healthy-lifestyle---who-recommendations
 
 import Decimal from 'decimal.js'
 
-function parseNutitionalStatus(bmi: number) {
-  if (bmi < 18.5) {
-    return 'Underweight'
-  }
-  else if (bmi < 25.0) {
-    return 'Normal weight'
-  }
-  else if (bmi < 30.0) {
-    return 'Pre-obesity'
-  }
-  else if (bmi < 35.0) {
-    return 'Obesity class I'
-  }
-  else if (bmi < 40.0) {
-    return 'Obesity class II'
-  }
-  else {
-    return 'Obesity class III'
-  }
-}
-
 function calculateNormalWeightRange(
   height: Decimal, // must m
+  bmiMin: number,
+  bmiMax: number,
 ) {
   const heightSquared = height.pow(2)
 
-  // Normal BMI range is 18.5 to 24.9
-  const minNormalWeight = heightSquared.mul(18.5)
-  const maxNormalWeight = heightSquared.mul(24.9)
+  // Normal BMI range is `bmiMin` to `bmiMax`
+  const minNormalWeight = heightSquared.mul(bmiMin)
+  const maxNormalWeight = heightSquared.mul(bmiMax)
 
   return {
-    min: Math.round(minNormalWeight.toNumber() * 10) / 10, // round to 1 decimal
-    max: Math.round(maxNormalWeight.toNumber() * 10) / 10,
+    min: minNormalWeight.toNumber(),
+    max: maxNormalWeight.toNumber(),
   }
 }
 
-function calc(props: {
+function apac(props: {
   // age?: number
-  sex: 'male' | 'female'
-  height: number | string // must cm
-  weight: number | string // must kg
+  // sex: 'male' | 'female'
+  height: Decimal // must m
+  weight: Decimal // must kg
 }) {
-  const _height = new Decimal(props.height).div(100) // convert to m
-  const _weight = new Decimal(props.weight)
+  function parseNutitionalStatus(bmi: number) {
+    if (bmi < 18.5) {
+      return 'Underweight'
+    }
+    else if (bmi < 23.0) {
+      return 'Normal weight'
+    }
+    else if (bmi < 25.0) {
+      return 'At risk'
+    }
+    else if (bmi < 30.0) {
+      return 'Obesity class I'
+    }
+    else {
+      return 'Obesity class II'
+    }
+  }
 
   // set default age if not defined
   // if (!props.age) {
@@ -59,8 +57,8 @@ function calc(props: {
 
   // throw
   // if (props.age >= 20) {
-  const bmi = _weight.div(_height.pow(2))
-  const normalWeightRange = calculateNormalWeightRange(_height)
+  const bmi = props.weight.div(props.height.pow(2))
+  const normalWeightRange = calculateNormalWeightRange(props.height, 18.5, 22.9)
 
   return {
     bmi: bmi.toNumber(),
@@ -73,6 +71,99 @@ function calc(props: {
   // }
 
   // throw new Error('not handled yet!')
+}
+
+function who(props: {
+  // age?: number
+  // sex: 'male' | 'female'
+  height: Decimal // must m
+  weight: Decimal // must kg
+}) {
+  function parseNutitionalStatus(bmi: number) {
+    if (bmi < 18.5) {
+      return 'Underweight'
+    }
+    else if (bmi < 25.0) {
+      return 'Normal weight'
+    }
+    else if (bmi < 30.0) {
+      return 'Pre-obesity'
+    }
+    else if (bmi < 35.0) {
+      return 'Obesity class I'
+    }
+    else if (bmi < 40.0) {
+      return 'Obesity class II'
+    }
+    else {
+      return 'Obesity class III'
+    }
+  }
+
+  // set default age if not defined
+  // if (!props.age) {
+  //   props.age = 20
+  // }
+
+  // throw
+  // if (props.age <= 0) {
+  //   throw new Error('age must be greater than 0')
+  // }
+
+  // throw
+  // if (props.age >= 20) {
+  const bmi = props.weight.div(props.height.pow(2))
+  const normalWeightRange = calculateNormalWeightRange(props.height, 18.5, 24.9)
+
+  return {
+    bmi: bmi.toNumber(),
+    status: parseNutitionalStatus(bmi.toNumber()),
+    normalWeightRange: {
+      min: normalWeightRange.min, // in kg
+      max: normalWeightRange.max, // in kg
+    },
+  }
+  // }
+
+  // throw new Error('not handled yet!')
+}
+
+function calc(
+  standard: 'APAC' | 'WHO',
+  props: {
+    // age?: number
+    // sex: 'male' | 'female'
+    height: number | string // must cm
+    weight: number | string // must kg
+  },
+): {
+    error?: string
+    data?: {
+      bmi: number
+      status: string
+      normalWeightRange: {
+        min: number // in kg
+        max: number // in kg
+      }
+    }
+  } {
+  try {
+    const _height = new Decimal(props.height).div(100) // convert to m
+    const _weight = new Decimal(props.weight)
+
+    switch (standard) {
+      case 'APAC':
+        return { data: apac({ ...props, height: _height, weight: _weight }) }
+      case 'WHO':
+      default:
+        return { data: who({ ...props, height: _height, weight: _weight }) }
+    }
+  }
+  catch (e) {
+    return {
+      error: e as string,
+    }
+  }
 }
 
 export { calc }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,50 +1,90 @@
 import { describe, expect, it } from 'vitest'
 import { calc } from '../src'
 
-describe('test BMI Calculator', () => {
+describe('test BMI Calculator (APAC)', () => {
   it('should calculate BMI and status for normal weight (number input)', () => {
-    const result = calc({ sex: 'male', height: 170, weight: 65 })
-    expect(result.bmi).toBeCloseTo(22.49, 2)
-    expect(result.status).toBe('Normal weight')
-    expect(result.normalWeightRange).toEqual({
-      min: 53.5,
-      max: 72,
+    const result = calc('APAC', { height: 170, weight: 65 })
+    expect(result?.data?.bmi).toBeCloseTo(22.49, 2)
+    expect(result?.data?.status).toBe('Normal weight')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 66.181,
+      min: 53.465,
+
     })
   })
 
   it('should calculate BMI and status for underweight', () => {
-    const result = calc({ sex: 'female', height: 160, weight: 45 })
-    expect(result.bmi).toBeCloseTo(17.58, 2)
-    expect(result.status).toBe('Underweight')
-    expect(result.normalWeightRange).toEqual({
-      min: 47.4,
-      max: 63.7,
+    const result = calc('APAC', { height: 160, weight: 45 })
+    expect(result?.data?.bmi).toBeCloseTo(17.58, 2)
+    expect(result?.data?.status).toBe('Underweight')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 58.624,
+      min: 47.36,
     })
   })
 
   it('should calculate BMI and status for obesity class II', () => {
-    const result = calc({ sex: 'male', height: 180, weight: 130 })
-    expect(result.bmi).toBeCloseTo(40.12, 2)
-    expect(result.status).toBe('Obesity class III') // BMI > 40
-    expect(result.normalWeightRange).toEqual({
-      min: 59.9,
-      max: 80.7,
+    const result = calc('APAC', { height: 180, weight: 130 })
+    expect(result?.data?.bmi).toBeCloseTo(40.12, 2)
+    expect(result?.data?.status).toBe('Obesity class II')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 74.196,
+      min: 59.94,
     })
   })
 
   it('should support string inputs', () => {
-    const result = calc({ sex: 'female', height: '165', weight: '70' })
-    expect(result.bmi).toBeCloseTo(25.71, 2)
-    expect(result.status).toBe('Pre-obesity')
-    expect(result.normalWeightRange).toEqual({
-      min: 50.4,
-      max: 67.8,
+    const result = calc('APAC', { height: '165', weight: '70' })
+    expect(result?.data?.bmi).toBeCloseTo(25.71, 2)
+    expect(result?.data?.status).toBe('Obesity class I')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 62.34525,
+      min: 50.36625,
+    })
+  })
+})
+
+describe('test BMI Calculator (WHO)', () => {
+  it('should calculate BMI and status for normal weight (number input)', () => {
+    const result = calc('WHO', { height: 170, weight: 65 })
+    expect(result?.data?.bmi).toBeCloseTo(22.49, 2)
+    expect(result?.data?.status).toBe('Normal weight')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 71.961,
+      min: 53.465,
     })
   })
 
-  it('should round normal weight range to 1 decimal', () => {
-    const result = calc({ sex: 'male', height: 175, weight: 75 })
-    expect(result.normalWeightRange.min % 0.1).toBe(0.0999999999999997)
-    expect(result.normalWeightRange.max % 0.1).toBe(0.09999999999999293)
+  it('should calculate BMI and status for underweight', () => {
+    const result = calc('WHO', { height: 160, weight: 45 })
+    expect(result?.data?.bmi).toBeCloseTo(17.58, 2)
+    expect(result?.data?.status).toBe('Underweight')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 63.744,
+      min: 47.36,
+
+    })
+  })
+
+  it('should calculate BMI and status for obesity class II', () => {
+    const result = calc('WHO', { height: 180, weight: 130 })
+    expect(result?.data?.bmi).toBeCloseTo(40.12, 2)
+    expect(result?.data?.status).toBe('Obesity class III')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 80.676,
+      min: 59.94,
+
+    })
+  })
+
+  it('should support string inputs', () => {
+    const result = calc('WHO', { height: '165', weight: '70' })
+    expect(result?.data?.bmi).toBeCloseTo(25.71, 2)
+    expect(result?.data?.status).toBe('Pre-obesity')
+    expect(result?.data?.normalWeightRange).toEqual({
+      max: 67.79025,
+      min: 50.36625,
+
+    })
   })
 })
